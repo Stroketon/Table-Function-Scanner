3f6102b80cbb2b23015c12f5705e101e48f025ba
Unverified <unverifiedlol@yahoo.com>
2020-04-29 02:41:35 -0400

    Update Scanner.lua

--- Scanner.lua
+++ Scanner.lua
@@ -1,4 +1,6 @@
-local function Return(i, v)
+local Scanner = {}
+
+function Scanner:Return(i, v)
     local v = v or "getupvalues"
     if typeof(i) == "table" then
         return i
@@ -9,12 +11,12 @@ local function Return(i, v)
     end
 end
 
-local function FindLocal(i, v)
+function Scanner:Scan(i, v)
     for i1, v1 in next, getgc() do
         if tostring(i1) == i and v then return getgc() elseif tostring(i1) == i then return v1 end
-        for i2, v2 in next, Return(v1) do
+        for i2, v2 in next, Scanner:Return(v1) do
             if tostring(i2) == i and v then return v1 elseif tostring(i2) == i then return v2 end
-            for i3, v3 in next, Return(v2) do
+            for i3, v3 in next, Scanner:Return(v2) do
                 if tostring(i3) == i and v then return v2 elseif tostring(i3) == i then return v3 end
             end
         end
